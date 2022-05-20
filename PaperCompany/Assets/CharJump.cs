b002fef7e64e12c3caed5e8985da9e08e027e7dd
khsd386 <81413499+khsd386@users.noreply.github.com>
2022-05-20 20:26:05 +0900

    comment
    코드에 주석문 추가했습니다 !

--- PaperCompany/Assets/CharJump.cs
+++ PaperCompany/Assets/CharJump.cs
@@ -4,18 +4,20 @@ using UnityEngine;
 
 public class CharJump : MonoBehaviour
 {
-    Rigidbody2D rb;
-    public float jumpPower;
+    Rigidbody2D rb;            // Rigidbody2D 클래스를 가진 변수 rb
+    public float jumpPower;    // jumpPower변수 공개 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();
+        rb = GetComponent<Rigidbody2D>(); 
+        // Rigidbody2D라는 컴포넌트를 rb에 넣겠다.
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0)) // 0 = Mouse left
+            // 마우스 좌클릭을 할 때 힘을 받아서 위로 움직이겠다
         {
             rb.velocity = Vector2.up * jumpPower; 
         }
